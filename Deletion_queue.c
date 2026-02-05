02cc8cc33ffa2f3c8330b8cea9f0017f8af5771f
Rudraksh Raina <rudraksh.raina.official@gmail.com>
2026-02-05 14:24:08 +0530

    Update Deletion_queue.c

--- Deletion_queue.c
+++ Deletion_queue.c
@@ -1,18 +1,29 @@
 #include <stdio.h>
 
 int main() {
-    int queue[10] = {10, 20, 30};
-    int front = 0, rear = 2;
+    int queue[10];
+    int front = 0, rear = -1;
+    int n, i;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    for (i = 0; i < n; i++) {
+        rear++;
+        printf("Enter element: ");
+        scanf("%d", &queue[rear]);
+    }
 
     if (front > rear) {
         printf("Queue is empty");
-    } else {
+    } 
+    else {
         printf("Deleted element is: %d\n", queue[front]);
         front++;
     }
 
     printf("Queue after deletion:\n");
-    for (int i = front; i <= rear; i++) {
+    for (i = front; i <= rear; i++) {
         printf("%d ", queue[i]);
     }
 
